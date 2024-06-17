2307d2f9bfc500e7861c3dc60d138c4dc64fabb8
ballcap <aalikhan1988@gmail.com>
2024-06-17 16:11:43 +0900

    #

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import PostImage from './PostImage.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <PostImage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
